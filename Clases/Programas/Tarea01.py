3a861434e24919e6c31b79983618a1443f5e2d0f
312080367 <asaped96@gmail.com>
2019-01-10 16:58:40 -0600

    Tarea01

---
+++ Clases/Programas/Tarea01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Jorge S. Martinez Villafan
+#A qué velocidad debe circular un auto de carreras para recorrer 50 km en un 1/4, 1/2 y una hora?
+
+
+print 50.0/0.25
+print 50.0/0.5
+print 50.0/1
+d= 50 
+t= 0.25, 0.5, 1.
+V= d/t
