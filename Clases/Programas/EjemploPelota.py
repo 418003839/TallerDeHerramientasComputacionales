7b8c28d765de7236b5f5c38618572fa93b23200f
312080367 <asaped96@gmail.com>
2019-01-10 16:32:03 -0600

    Actualicé el ejercicio

--- Clases/Programas/EjemploPelota.py
+++ Clases/Programas/EjemploPelota.py
@@ -1,2 +1,16 @@
-print 34*3 - 1.0/2 *9.8*3**2
+# -*- coding: utf-8 -*-
+# Jorge S- Martínez Villafan
+print 34*3 - 1/2 *9.8*3**2 #1/2 calcula la division entera
+print 34*3 - 1.0/2 *9.8*3**2 #1.0/2 división flotante "real"
 
+print 34*1 - 1.0/2 *9.8*1**2
+
+print 34*1.5 - 1.0/2 *9.8*1.5**2
+
+print 34*5 - 1.0/2 *9.8*5**2
+
+v0 = 34
+g = 9.81
+t = 5
+y = v0*t - 1.0/2*g*t**2
+print y
