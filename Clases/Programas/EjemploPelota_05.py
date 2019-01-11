0c689783ae1fa1266aa3fa86bba109980c74c272
418003839 <Jorge_SM2@ciencias.unam.mxgit config --global user.email Jorge_SM2@ciencias.unam.m>
2019-01-11 16:03:31 -0600

    asdasd

--- Clases/Programas/EjemploPelota_05.py
+++
@@ -1,9 +0,0 @@
-
-v0 = 34
-g = 9.81
-t = 4.3
-y = v0*t - 1.0/2*g*t**2
-print 'La posición de la pelota en el t=%E es %10.7f\n%f' % (t,y,t)
-def posicion(t,v0):
-    y = v0*t - 1.0/2*g*t**2
-    return(y)
