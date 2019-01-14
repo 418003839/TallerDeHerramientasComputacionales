9efeb9bb7148ea8c14999ab4d5119f547e5c0d33
418003839 <Jorge_SM2@ciencias.unam.mxgit config --global user.email Jorge_SM2@ciencias.unam.m>
2019-01-14 15:45:19 -0600

    La tarea numero dos

---
+++ Clases/Programas/Tarea2.py
@@ -0,0 +1,6 @@
+#Tarea numero dos, basada en la tarea numero uno.
+def velocidad(d,t):
+    v= float(d)/t
+    return v
+#Ejemplo: Calcular la velocidad de un auto de carreras para rcorrer 20 km en .02 horas
+print (velocidad(20,0.20))
