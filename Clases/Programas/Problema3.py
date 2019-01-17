213d67d684f383cd30ab6a728e7be0604fb2c8a2
418003839 <46503285+418003839@users.noreply.github.com>
2019-01-17 17:34:22 +0000

    Delete Problema3.py

--- Clases/Programas/Problema3.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding utf-8 _*_
-C = int(input("grados en celsius:"))
-F = 1.8 * C + 32
-print(C, "grados celsius en farenheit son: ",F,"")
-        
-
-F = int(input("grados en farenheit:"))
-C = (F - 32)/1.8
-print(F, "grados celsius en farenheit son: ",C,"")
