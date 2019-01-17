0017daa2ece59a7845198ea8cabb535bcfdb2994
418003839 <46503285+418003839@users.noreply.github.com>
2019-01-17 17:35:02 +0000

    Delete Problema5S.py

--- Clases/Programas/Problema5S.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding utf-8 _*_
-#Calcular la suma desde i=1 hasta n de i
-#Calcule la suma de 1+2...+100
-def sumar(n):
-    return (n* (n+1))/2
-n = int(input("Numero de naturales a sumar"))
-suma = sumar (n)
-print ("resultado: ", suma)
-
