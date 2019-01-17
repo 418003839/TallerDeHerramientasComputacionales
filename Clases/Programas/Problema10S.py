c82b46c206326339da21b5a5e5efffc412358931
418003839 <46503285+418003839@users.noreply.github.com>
2019-01-17 17:31:50 +0000

    Delete Problema10S.py

--- Clases/Programas/Problema10S.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding utf-8 _*_
-#Programa para saber si un alumno aprobó o reprobó
-Calificación =int(input("calificación: "))
-if Calificación>=6:
-        print("Aprobado")
-
-else:
-        print ("reprobado")
