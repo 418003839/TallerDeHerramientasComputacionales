3419151f2c0c64625275b34d0f9b9669948ff4cd
418003839 <46503285+418003839@users.noreply.github.com>
2019-01-17 17:33:21 +0000

    Delete Problema4S.py

--- Clases/Programas/Problema4S.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding utf-8 _*_
-#Calcule el termino numero 20 de la suceción de fibonacci
-def fibonacci(n):
-    if n<=1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
- #Para cácular el valor n de la sucecion de fibonacci basta correr el codigo y suplir la n por el valor que deseemos
-#Ejemplo fibonacci(19) = 4181
