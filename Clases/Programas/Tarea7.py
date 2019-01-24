e2805d9e5f9c0b2a860d591d95b855abe60f76c6
418003839 <Jorge_SM25@ciencias.unam.mx>
2019-01-24 11:01:46 -0600

    Tarea7

---
+++ Clases/Programas/Tarea7.py
@@ -0,0 +1,11 @@
+Lista = [1,2,3,4,5,6,2,4,2,13,5]
+print (Lista.copy())
+Lista.sort()
+print (Lista)
+Lista.reverse()
+print (Lista)
+print (Lista.count(2))
+Lista.remove(5)
+Lista.remove(4)
+print (Lista)
+
---
+++ Clases/Programas/Tarea7S.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.6.7
+# _*_ coding utf-8 _*_
+"""
+Jorge S Martínez Villafan, 418003839
+Taller de herraientas computacionales
+Problando el resto de las funciones de las listas
+"""
+#De la siguienete Lista = [1,2,3,4,5,6,2,4,2,13,5]
+#Cree una copia
+#Ordenela numericamente
+#Invierta el orden de ella
+#Cuente cuantos 2 hay
+#Remueva los numeros 5 y 4 de ella
+from Tarea7 import *
